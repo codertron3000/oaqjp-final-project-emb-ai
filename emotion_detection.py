3593eb3b38f061fd0cd543211381a36c8f07705d
codertron3000 <judah.purcell@gmail.com>
2024-03-31 20:56:54 -0400

    working on dom emotion logic

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,26 +8,21 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-    def highest_score(options):
-        result = {'emotion': '', 'score': 0}
-        for item in options:
-            if item > result['score']:
-                result['emotion'] = item1
-            else:
-                result = item2
-        return result
-
     result = {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score,
-        'dominant_emotion': dominant_emotion,
+        'anger': formatted_response['emotionPredictions'][0]['emotion']['anger'],
+        'disgust': formatted_response['emotionPredictions'][0]['emotion']['disgust'],
+        'fear': formatted_response['emotionPredictions'][0]['emotion']['fear'],
+        'joy': formatted_response['emotionPredictions'][0]['emotion']['joy'],
+        'sadness': formatted_response['emotionPredictions'][0]['emotion']['sadness'],
+        'dominant_emotion': '',
     }
+    def find_dominant_emotion(options):
+        dominant = {'emotion': '', 'score': 0}
+        for key, value in options:
+            if value > dominant ['score']:
+                dominant ['emotion'] = key
+                dominant ['score'] = value
+        result['dominant_emotion'] = dominant['emotion']
+        return
+    find_dominant_emotion(result)
     return result
