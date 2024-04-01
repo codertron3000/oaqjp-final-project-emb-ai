1bedf734122be55d2b2fb126ad5dc4d0c0487fca
codertron3000 <judah.purcell@gmail.com>
2024-04-01 15:22:44 -0400

    complete to the end of task 7

--- server.py
+++ server.py
@@ -14,6 +14,8 @@ def render_index_page():
 def emot_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
     return f"""for the given statement, the system response is 'anger': {response['anger']},
     'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']},
     'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}"""
