3da1bcc78b1f70b7368a2d7f4216c6053347b96f
codertron3000 <judah.purcell@gmail.com>
2024-04-01 09:45:12 -0400

    complete up to task 4, #3

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,17 +9,17 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
     result = {
-        'anger': formatted_response['emotionPredictions'][0]['emotion']['anger'],
-        'disgust': formatted_response['emotionPredictions'][0]['emotion']['disgust'],
-        'fear': formatted_response['emotionPredictions'][0]['emotion']['fear'],
-        'joy': formatted_response['emotionPredictions'][0]['emotion']['joy'],
-        'sadness': formatted_response['emotionPredictions'][0]['emotion']['sadness'],
-        'dominant_emotion': '',
+        'anger': float(formatted_response['emotionPredictions'][0]['emotion']['anger']),
+        'disgust': float(formatted_response['emotionPredictions'][0]['emotion']['disgust']),
+        'fear': float(formatted_response['emotionPredictions'][0]['emotion']['fear']),
+        'joy': float(formatted_response['emotionPredictions'][0]['emotion']['joy']),
+        'sadness': float(formatted_response['emotionPredictions'][0]['emotion']['sadness']),
     }
     def find_dominant_emotion(options):
-        dominant = {'emotion': '', 'score': 0}
-        for key, value in options:
-            if value > dominant ['score']:
+        dominant = {'emotion': '', 'score': 0.0}
+        print(dominant['score'])
+        for key, value in options.items():
+            if value > dominant['score']:
                 dominant ['emotion'] = key
                 dominant ['score'] = value
         result['dominant_emotion'] = dominant['emotion']
